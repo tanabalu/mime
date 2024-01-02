771ec23331a69ea206dc4440c2cc9f763b2fe23d
满昌 <cx362562@antgroup.com>
2024-01-02 11:41:20 +0800

    build: config

--- .umirc.ts
+++ .umirc.ts
@@ -1,6 +1,8 @@
 import { defineConfig } from "umi";
 
 export default defineConfig({
+  base: '/mime/',
+  publicPath: '/mime/',
   routes: [
     { path: "/", component: "index" },
     { path: "/docs", component: "docs" },
