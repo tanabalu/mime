d0c8f6f817771be95e954db76d3dd3efa490b539
满昌 <cx362562@antgroup.com>
2024-01-02 15:08:47 +0800

    style: delete

--- .umirc.ts
+++ .umirc.ts
@@ -1,21 +1,10 @@
 import { defineConfig } from "umi";
 
 export default defineConfig({
-  // base: '/mime',
-  // publicPath: '/mime/',
-  // base: '.',
-  // publicPath: '/mime/',
-  // base: process.env.NODE_ENV === 'production' ? '/mime' : '/',
-  // publicPath: process.env.NODE_ENV === 'production' ? './' : '/',
   publicPath: process.env.NODE_ENV === 'production' ? '/mime/' : '/',
-  // publicPath: 'https://tanabalu.github.io/mime/',
   routes: [
     { path: "/", component: "./Home" },
-    // { path: "/docs", component: "docs" },
   ],
-  // codeSplitting: {
-  //   jsStrategy: 'granularChunks',
-  // },
   npmClient: 'npm',
   history: { type: 'hash' },
   hash: true,
